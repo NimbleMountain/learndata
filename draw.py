193d973651447a6c4d35789d398ce4234c7f7766
NimbleMountain <Nimble.Mountain@gmail.com>
2017-10-19 14:59:05 -0700

    Delete draw.py

--- draw.py
+++
@@ -1,5 +0,0 @@
-
-# This file will have a simple python based chart drawn using some undetermined set of data science libraries/tools
-# Currently need to establish which tools to use
-# also need to choose a data source.  First a static sample, but the goal being a sources that updates via an API
-
